4dcaeda50f96a15ecac41812832f385d0bbcbd02
nishantsakesh@gmail.com <nishantsakesh@gmail.com>
2026-09-09 11:44:25 +0530

    Day 2: Spring Boot Setup and Docker Infrastructure

---
+++ src/main/java/com/webhook/engine/WebhookEngineApplication.java
@@ -0,0 +1,12 @@
+package com.webhook.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookEngineApplication.class, args);
+    }
+}
